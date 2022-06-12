967ff6cf0926550b7ee149affb43f0f54913021f
firestonelib <firestonelib@gmail.com>
2022-06-12 16:00:53 +0800

    add in-1k script

--- beit/in-1k_lin.sh
+++
@@ -1,8 +0,0 @@
-GLOG_v=0 python3.7 -m torch.distributed.launch \
-    --nproc_per_node=8 \
-    run_linear_eval.py \                                                                                                                                                                                    
-    --model beit_base_patch16_224 \
-    --pretrained_weights beit_base_ep800.pt \
-    --data_path ../../../logs \
-    --lr 4e-3 \
-    --output_dir output >beit_base_ep800lin.log 2>&1 & 
